5db02af822b668e4dbff608fcbcdea5e0133834c
duckserver <79085028+duckserver@users.noreply.github.com>
2021-07-24 23:31:48 +0900

    Create 2675.c
    question source : https://www.acmicpc.net/problem/2675

---
+++ 2675.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS    
+#include <stdio.h>
+#include <string.h>  
+
+int main()
+{
+	char s[21];
+	int t;
+	int r;
+
+	scanf("%d", &t);
+	
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d %s", &r, s);
+		for (int j = 0; j < strlen(s); j++)
+		{
+			for (int k = 0; k < r; k++)
+				printf("%c", s[j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
